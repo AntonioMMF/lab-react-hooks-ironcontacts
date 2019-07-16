7b1ac9bd9c2f975d7076bef92ae11db0d9db100a
António Mesquita e Ferreira <antonio@Antonios-MBP.lan>
2019-07-16 19:32:45 +0100

    some commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Table } from "reactstrap";
+import logo from "./logo.svg";
+import "./App.css";
+import fullListOfContacts from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(fullListOfContacts.slice(0, 5));
+  const [contactId, setcontactId] = useState(0);
+
+  function handleClick() {
+    // let random = Math.floor(Math.random(useState(fullListOfContacts)))
+    // setcontactId(random);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Olá</h1><br/>
+        <button className="btn btn-primary" onClick={handleClick}>One More Random Dude</button> <br /><br/>
+        <Table hover> 
+          <thread>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thread>
+          <tbody>
+            {contacts.map(c => (
+              <tr key={c.id}>
+                <td><img src={c.pictureUrl} alt="" width="100"/></td>
+                <td>{c.name}</td>
+                <td>{c.popularity}</td>
+
+                
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
